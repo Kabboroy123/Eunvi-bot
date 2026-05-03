d9ee4c85bfaa907b157e59d48ebb02130dce5c11
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 22:34:48 +0530

    Update credits in tid.js

--- Script/commands/tid.js
+++ Script/commands/tid.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "tid",
   version: "2.0.0",
   hasPermssion: 0,
-  credits: "SHAHADAT SAHU",
+  credits: "kabbo 420",
   description: "Show current thread ID",
   commandCategory: "group",
   usages: "tid",
