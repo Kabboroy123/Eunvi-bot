344eee3599ece4a4616623f2658526e8ac635f88
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 22:45:17 +0530

    Update credits in slap command configuration

--- Script/commands/slap.js
+++ Script/commands/slap.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "slap",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "Eunvi ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
   description: "Slap the friend tag",
   commandCategory: "general",
   usages: "slap [Tag someone you want to slap]",
