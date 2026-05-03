175694461b4e907ae0722d86b6b7e8da23e33274
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:45:26 +0530

    ,

--- Script/commands/dog.js
+++ Script/commands/dog.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "dog",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Eunvi ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Xem Boss",
 	commandCategory: "Picture",
 	usages: "dog [Text]",
