cf4d3e586f50a10d24578eafaed877dec6b23fcb
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 22:23:00 +0530

    Update credits in weather command configuration

--- Script/commands/weather.js
+++ Script/commands/weather.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "weather",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Eunvi ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "See weather information in the area",
 	commandCategory: "other",
 	usages: "[Location]",
