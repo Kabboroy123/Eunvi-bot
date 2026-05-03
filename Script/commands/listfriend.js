852297886a46a8b201d5b339be76adf960cef697
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:26:36 +0530

    Update credits in listfriend.js

--- Script/commands/listfriend.js
+++ Script/commands/listfriend.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "listfriend",
   version: "1.0.0",
   hasPermssion: 2,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "Eunvi☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
   description: "View friends information/Delete friends by replying",
   commandCategory: "System",
   usages: "",
