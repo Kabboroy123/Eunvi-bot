985ef1ef266d148727af0ce4263f6f66cd78fd62
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:51:52 +0530

    Update credits in broken.js

--- Script/commands/broken.js
+++ Script/commands/broken.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "broken",
  version: "1.0.0",
  hasPermssion: 2,
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ credits: "Eunvi ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
  description: "5 বারের জন্য ক্রমাগত বন্ধুর ট্যাগ ট্যাগ করুন\nসেই ব্যক্তিকে আত্মা কলিং বলা যেতে পারে",
  commandCategory: "nsfw",
  usages: "please @mention",
