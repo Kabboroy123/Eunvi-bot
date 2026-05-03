d599cd5d197e88ea1a7fe353fe28bfc9f17fcc89
Kabboroy123 <kabboroy177@gmail.com>
2026-05-04 00:09:44 +0530

    Update credits in album.js

--- Script/commands/album.js
+++ Script/commands/album.js
@@ -13,7 +13,7 @@ module.exports.config = {
  name: "album",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "Dipto Modified By SAHU", 
+ credits: "moriom Modified By 420", 
  description: "Displays album options for selection.",
  usePrefix: true,
  prefix: true,
