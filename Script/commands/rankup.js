e7dc350295201489e2058a54e82c23713a7626dd
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:06:52 +0530

    Update credits for rankup command

--- Script/commands/rankup.js
+++ Script/commands/rankup.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "rankup",
   version: "2.0.0",
   hasPermssion: 1,
-  credits: "SHAHADAT SAHU",
+  credits: "kabbo 420",
   description: "Announce rankup for each group/user",
   commandCategory: "Edit-IMG",
   dependencies: { "fs-extra": "" },
