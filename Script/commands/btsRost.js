0639a2c12508b5f3abdbad52c5522f6bfa51fa50
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:51:15 +0530

    Update btsRost.js

--- Script/commands/btsRost.js
+++ Script/commands/btsRost.js
@@ -2,7 +2,7 @@ module.exports.config = {
   'name': "bts",
   'version': "1.0.2",
   'hasPermission': 0x2,
-  'credits': "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  'credits': "Eunvi ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
   'description': '',
   'commandCategory': "admin",
   'usages': "[tag]",
