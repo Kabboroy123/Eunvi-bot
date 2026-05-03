b5a764333675e06ec3e0acf84794d4b4539a60c3
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:55:11 +0530

    Update credits in birthday command

--- Script/commands/birthday.js
+++ Script/commands/birthday.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "birthday",
  version: "1.0.0",
  hasPermssion: 2,
- credits: "Shahadat SAHU",
+ credits: "kabbo 420",
  description: "মেনশন করলে শুভেচ্ছা জানাবে",
  commandCategory: "group",
  usages: "[@মেনশন]",
