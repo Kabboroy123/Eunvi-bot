a2d077e0a8c54e25f85b6daefaf5e50d45f7a215
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:00:23 +0530

    Update credits in restart command configuration

--- Script/commands/restrat.js
+++ Script/commands/restrat.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "restart",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Eunvi ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Restart Bot",
 	commandCategory: "system",
 	usages: "",
