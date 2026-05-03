32b13c7a08e819394836f31316f64aae147a4d0b
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 22:44:17 +0530

    Update credits for slot command

--- Script/commands/slot.js
+++ Script/commands/slot.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "slot",
     version: "1.1.0",
     hasPermssion: 0,
-    credits: "SHAHADAT SAHU",
+    credits: "kabbo 420",
     description: "slot machine game",
     commandCategory: "game",
     usages: "[bet amount]",
