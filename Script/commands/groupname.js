b5362888064176e9b80995340846fa29fde6f201
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:34:59 +0530

    Update credits in groupname command

--- Script/commands/groupname.js
+++ Script/commands/groupname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "groupname",
 	version: "1.0.0", 
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Eunvi ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Rename your group",
 	commandCategory: "Box", 
 	usages: "groupname [name]", 
