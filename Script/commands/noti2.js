3321a9055e74a42c609b29abcdf93b509b09f013
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:20:21 +0530

    Update credits in noti2.js

--- Script/commands/noti2.js
+++ Script/commands/noti2.js
@@ -4,7 +4,7 @@ module.exports.config = {
  'name': "noti2",
  'version': "1.0.0",
  'hasPermssion': 0x2,
- 'credits': "MAHBUB SHAON",
+ 'credits': "mariom",
  'description': '',
  'commandCategory': "sandnoto",
  'usages': "[msg]",
