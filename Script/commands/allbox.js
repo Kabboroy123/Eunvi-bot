da64e2b2abe27aba979ee923b37107b539c1b261
Kabboroy123 <kabboroy177@gmail.com>
2026-05-04 00:07:55 +0530

    Update credits in allbox.js

--- Script/commands/allbox.js
+++ Script/commands/allbox.js
@@ -1,7 +1,7 @@
 module.exports.config = {
  name: 'allbox',
  version: '1.0.0',
- credits: '𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️',
+ credits: 'Eunvi ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️',
  hasPermssion: 2,
  description: '[Ban/Unban/Del/Remove] List[Data] thread The bot has joined in.',
  commandCategory: 'Admin',
