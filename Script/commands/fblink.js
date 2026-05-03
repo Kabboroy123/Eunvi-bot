9addbcd46e4755df1ca01f423bb986bcfdeca47f
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:43:39 +0530

    Update fblink.js

--- Script/commands/fblink.js
+++ Script/commands/fblink.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "fblink",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "Shahadat SAHU",
+	credits: "kabbo 420",
 	description: "Get linkfb.",
 	commandCategory: "Generate FB id link",
 	cooldowns: 5
