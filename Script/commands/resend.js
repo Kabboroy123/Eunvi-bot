56388bcc7900953e32e5333cc4c5efe5e8a2822f
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:01:17 +0530

    Update resend.js

--- Script/commands/resend.js
+++ Script/commands/resend.js
@@ -5,7 +5,7 @@ module.exports.config = {
   name: "resend",
   version: "1.1.0",
   hasPermssion: 0,
-  credits: "SHAHADAT SAHU",
+  credits: "kabbo 420",
   description: "Auto resend removed messages",
   commandCategory: "general",
   cooldowns: 0,
