a668fa56ed6a2b717a2848ed2bf2ea06eda25e60
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 22:46:37 +0530

    Update credits in shortcut.js

--- Script/commands/shortcut.js
+++ Script/commands/shortcut.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "shortcut",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Enuvi ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "",
 	commandCategory: "system",
     usages: "[all/delete/empty]",
