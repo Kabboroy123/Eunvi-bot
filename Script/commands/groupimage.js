a1b85e499f97ab7171f18ec466bce9e44d2d9e61
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:35:46 +0530

    Update credits in groupimage.js

--- Script/commands/groupimage.js
+++ Script/commands/groupimage.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	name: "groupimage",
 	version: "1.0.0", 
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Eunvi ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Change your group image",
 	commandCategory: "Box", 
 	usages: "groupimage", 
