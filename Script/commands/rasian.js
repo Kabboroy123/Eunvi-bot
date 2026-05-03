e4dddd5a4c980b875285b5459b9b4a061875229a
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:05:36 +0530

    Update rasian.js

--- Script/commands/rasian.js
+++ Script/commands/rasian.js
@@ -6,7 +6,7 @@ module.exports.config = {
  name: "rasian",
  version: "1.0.2",
  hasPermission: 0,
- credits: "Shaon Ahmed",
+ credits: "moriom",
  description: "Send a random shoti (TikTok short video)",
  commandCategory: "media",
  usages: "",
