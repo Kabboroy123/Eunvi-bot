445e77e8563c5848f993d06c4867babbc237657f
Kabboroy123 <kabboroy177@gmail.com>
2026-05-03 23:48:28 +0530

    Update credits in config.js

--- Script/commands/config.js
+++ Script/commands/config.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "config",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Eunvi ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "config bot!",
 	commandCategory: "admin",
 	cooldowns: 5
